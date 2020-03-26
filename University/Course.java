9e383692f2650f8b14475eeb1d2fb01e28c3921c
Vincenzo <58368395+m-Vins@users.noreply.github.com>
2020-03-26 18:03:35 +0100

    Update Course.java

--- University/Course.java
+++ University/Course.java
@@ -4,17 +4,45 @@ public class Course {
 	private String Name;
 	private String Teacher;
 	private Integer Cod;
-	private final static int MAXSTUD=100;
 	private int[] Students;
+	private int nextCod;
+	private final static int MAXSTUD=100;
+	
 	
 	public Course(String Name, String Teacher,Integer Cod) {
 		this.Name=Name;
 		this.Teacher=Teacher;
 		this.Cod=Cod;
 		Students= new int[MAXSTUD];
+		this.nextCod=0;
 	}
 	
 	public String toString() {
 		return Cod.toString()+", " + Name +", " + Teacher;
+	}
+	/**
+	 * assign a new student to the course
+	 * @param cod ID of the student
+	 */
+	public void newStudent(int cod) {
+		this.Students[nextCod++]=cod;
+	}
+	
+	/**
+	 * 
+	 * @return the number of the students in the course
+	 */
+	
+	public int CountStudents() {
+		return nextCod;
+	}
+	
+	/**
+	 * 
+	 * @return the Array of the IDs of the student in the course
+	 */
+	
+	public int[] StudentsArray() {
+		return this.Students;
 	}
 }
