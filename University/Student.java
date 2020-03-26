d8eb3dc850eaaeaa622ac9d54440a055ae7dbe41
Vincenzo <58368395+m-Vins@users.noreply.github.com>
2020-03-26 18:03:18 +0100

    Update Student.java

--- University/Student.java
+++ University/Student.java
@@ -1,25 +1,51 @@
 package university;
 
+/**
+ * @author Master
+ *
+ */
 public class Student {
 	private String Name;
 	private String Surname;
 	private Integer Key; 
 	private int[] Courses;
+	private int nextCourse;
 	private static final int MAXCOURSES=25;
-	
+
 	
 	public Student(String first, String last,Integer key ) { 
 		this.Name=first;
 		this.Surname=last;
 		this.Key=key;
 		this.Courses= new int[MAXCOURSES];
+		this.nextCourse=0;
 	}
 	
 	public String toString() {
 		return Key.toString() + " " + Name + " " + Surname;
 	}
+	/**
+	 * assign a new course to the student
+	 * @param cod ID of the course
+	 */
+	public void newCourse(int cod) {
+		this.Courses[nextCourse++]=cod;
+	}
 	
-	public boolean cmp(int key) {
-		return this.Key.intValue()==key? true:false;
+	/**
+	 * 
+	 * @return the number of the student courses
+	 */
+	
+	public int CountCourses() {
+		return this.nextCourse;
+	}
+	
+	/**
+	 * 
+	 * @return the Array of the IDs of the student courses
+	 */
+	public int[] CoursesArray() {
+		return this.Courses;
 	}
 }
